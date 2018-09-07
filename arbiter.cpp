6449399d00227e41d3f78425bdbefd2cd410ccc5
Jacko Dirks <jacko.dirks@gmail.com>
2018-09-07 13:37:35 +0200

    The arbiter can now handle the full game state

--- arbiter.cpp
+++ arbiter.cpp
@@ -1,5 +1,22 @@
 #include "arbiter.h"
 
-bool Arbiter::cardIsAllowed(int curCard, int newCard){
+bool Arbiter::playIsAllowed(QList<QString> stack, QString proposedCard){
     return true;
+}
+
+struct NewPlayState Arbiter::newPlayState(QList<QString> stack, QList<QString> proposedCards){
+    struct NewPlayState state;
+    state.clearStack = false;
+    state.turnIncrement = 1;
+    return state;
+}
+
+
+QList<bool> Arbiter::playIsPossible(QList<QString> stack, QList<QString> proposedCards){
+    QList<bool> retList;
+    retList.reserve(proposedCards.size());
+    for (QString s : proposedCards){
+        retList.append(playIsAllowed(stack, s));
+    }
+    return retList;
 }
--- arbiter.h
+++ arbiter.h
@@ -1,6 +1,16 @@
-class Arbiter {
-    public:
-        bool cardIsAllowed(int curCard, int newCard);
-    private:
-    protected:
+#include <QList>
+
+struct NewPlayState {
+    bool clearStack;
+    int turnIncrement;
 };
+
+class Arbiter {
+    protected:
+        bool playIsAllowed(QList<QString> stack, QString proposedCard);
+    public:
+        struct NewPlayState newPlayState(QList<QString> stack, QList<QString> proposedCards);
+        QList<bool> playIsPossible(QList<QString> stack, QList<QString> proposedCards);
+    private:
+};
+
